fbd1df9d2f0e7150f802d76bf2f4b16cdfa21181
Anastasia Kogteva <kogteva.nastya@gmail.com>
2022-10-27 22:34:41 +0200

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/variables.scss";`,
-      },
-    },
-  },
+  plugins: [react()]
 })
