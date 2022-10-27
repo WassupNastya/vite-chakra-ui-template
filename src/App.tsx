d5c678bf04302253dd0b53480032062a6e5d4f0b
Anastasia Kogteva <kogteva.nastya@gmail.com>
2022-10-27 21:50:21 +0200

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { Admin } from './routes/admin'
+import { Root } from './routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+  {
+    path: '/admin',
+    element: <Admin />,
+  },
+])
 
 function App() {
   return (
     <ChakraProvider>
-      <div>Hello, undefined</div>
+      <RouterProvider router={router} />
     </ChakraProvider>
   )
 }
